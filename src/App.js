f7c5d909ce251e778eec236a9495e7dd43d48af4
spock166 <21161498+spock166@users.noreply.github.com>
2022-12-16 21:48:08 -0600

    implent app basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
+  const [headpats, setHeadpats] = useState(0);
+  const [name, setName] = useState("");
+  const [showNameEdit, setNameEdit] = useState(true);
+  const [color, setColor] = useState('#' + Math.floor(Math.random()*16777215).toString(16));
+
+  const increaseHeadpats =()=>{
+    setHeadpats(headpats+1);
+    setColor('#' + Math.floor(Math.random()*16777215).toString(16));
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <p><button onClick={()=>{setNameEdit(!showNameEdit)}}>Toggle Name Entry</button></p>
+      {showNameEdit && <input type="text"onChange={handleNameChange}/>}
+      <h2>Senpai {name}</h2>
+      <h1 style={{color:color}}>Headpats:{headpats} </h1>
+      <button onClick={increaseHeadpats}>Pat head</button>
+    </div>);
+};
+
+
 
 export default App;
