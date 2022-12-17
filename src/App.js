4b9de98b7009c53f447fbd6c2445dcfe8329e463
spock166 <21161498+spock166@users.noreply.github.com>
2022-12-16 23:00:45 -0600

    Update buying weebs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
   const [headpats, setHeadpats] = useState(0);
   const [name, setName] = useState("");
   const [showNameEdit, setNameEdit] = useState(true);
   const [color, setColor] = useState('#' + Math.floor(Math.random()*16777215).toString(16));
+  const [weebs, setWeebs] = useState(0);
+  const [requiredHeadpats, setRequiredHeadpats] = useState(10);
 
-  const increaseHeadpats =()=>{
+  const increaseHeadpats = ()=>{
     setHeadpats(headpats+1);
     setColor('#' + Math.floor(Math.random()*16777215).toString(16));
   }
@@ -16,16 +18,32 @@ function App() {
     setName(event.target.value);
   }
 
+  const buyWeeb = ()=>{
+    setHeadpats(headpats-requiredHeadpats);
+    setRequiredHeadpats(Math.floor(requiredHeadpats*1.2));
+    setWeebs(weebs+1);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeadpats(headpats+weebs);
+    }, 1000);
+  
+    return () => clearInterval(interval);
+  }, [headpats, weebs]);
+
   return(
     <div className="App">
       <p><button onClick={()=>{setNameEdit(!showNameEdit)}}>Toggle Name Entry</button></p>
       {showNameEdit && <input type="text"onChange={handleNameChange}/>}
-      <h2>{name} Senpai</h2>
+      <h2>{name?name:"Jimmy"}-senpai</h2>
       <h1 style={{color:color}}>Headpats:{headpats} </h1>
+      <p>Weebs: {weebs}</p>
+      <p>Headpats until next week: {Math.max(requiredHeadpats-headpats,0)}</p>
       <button onClick={increaseHeadpats}>Pat head</button>
+      <button disabled={headpats >= requiredHeadpats ?false:true} onClick={buyWeeb}>Buy Weeb</button>
     </div>);
 };
-
 
 
 export default App;
